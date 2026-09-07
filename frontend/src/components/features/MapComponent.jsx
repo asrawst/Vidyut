c14cf90d5094344917420a5840d0c5cd76e40919
asrawst <as.aditya@outlook.com>
2026-09-08 00:42:33 +0530

    fix: replace watermarked Carto map tiles with ESRI Dark Canvas

--- frontend/src/components/features/MapComponent.jsx
+++ frontend/src/components/features/MapComponent.jsx
@@ -166,8 +166,9 @@ const MapComponent = ({ data, focusedConsumerId, onSelectConsumer }) => {
                 style={{ height: '100%', width: '100%', minHeight: '380px', background: '#242f3e' }}
             >
                 <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-                    url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+                    attribution='&copy; <a href="https://www.esri.com/">Esri</a> &mdash; Esri, DeLorme, NAVTEQ'
+                    url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}"
+                    maxZoom={16}
                 />
 
                 <MapController 
